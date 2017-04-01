8d2537bcb7b96b59f768996a1325d78c97eed56e
gaoyihan <ygao34@siebl-1119-01.ad.uillinois.edu>
2017-04-01 13:58:28 -0500

    update

--- NodejsAddon/addon.cpp
+++ NodejsAddon/addon.cpp
@@ -14,7 +14,6 @@ using namespace v8;
     v8::String::Utf8Value param(args[0]->ToString());
     std::string str(*param);
     
-    std::vector<std::vector<std::string>> table_vec;
     std::string output_filename;
     ExtractStructFromString(str);
   
@@ -40,23 +39,78 @@ void ExtractStructFromFile(const FunctionCallbackInfo<Value>& args) {
     ExtractStructFromString(str);	
 }*/
 
+void NodeCandidateGen(const FunctionCallbackInfo<Value>& args) {
+	Isolate* isolate = args.GetIsolate();
+
+    v8::String::Utf8Value param1(args[0]->ToString());
+    std::string filename(*param1);
+	
+	// Candidate Gen
+	std::vector<std::string> vec, escaped;
+	CandidateGenerate(filename, &vec, &escaped);
+	
+	Local<Array> schema_list = Array::New(isolate);	
+	for (int i = 0; i < vec.size(); ++i)
+		schema_list->Set(i, String::NewFromUtf8(isolate, vec[i].c_str()));
+	
+	Local<Array> escaped_list = Array::New(isolate);
+	for (int i = 0; i < escaped.size(); ++i)
+		escaped_list->Set(i, String::NewFromUtf8(isolate, escaped[i].c_str()));
+
+	Local<Object> obj = Object::New(isolate);
+	obj->Set(String::NewFromUtf8(isolate, "schema_list"), schema_list);
+	obj->Set(String::NewFromUtf8(isolate, "escaped_list"), escaped_list);
+	
+	args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Hello World From CandidateGen!"));
+}
+
 void NodeSelectSchema(const FunctionCallbackInfo<Value>& args) {
 	Isolate* isolate = args.GetIsolate();
 	
-	args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Hello World From SelectSchema!"));
-}
-
-void NodeCandidateGen(const FunctionCallbackInfo<Value>& args) {
-	Isolate* isolate = args.GetIsolate();
+    v8::String::Utf8Value param1(args[0]->ToString());
+    std::string filename(*param1);
 	
-	args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Hello World From CandidateGen!"));
+	std::vector<std::string> schema_list;
+	Handle<Array> param2 = Handle<Array>::Cast(args[1]);
+	for (int i = 0; i < param2->Length(); ++i) {
+		v8::String::Utf8Value item(param2->Get(i)->ToString());	
+		std::string schema(*item);
+		schema_list.push_back(schema);
+	}
+	
+	// Select Schema
+	std::string schema, escaped;
+	SelectSchema(filename, schema_list, &schema, &escaped);
+	
+	Local<Object> obj = Object::New(isolate);
+	obj->Set(String::NewFromUtf8(isolate, "schema"), String::NewFromUtf8(isolate, schema.c_str()));
+	obj->Set(String::NewFromUtf8(isolate, "escaped"), String::NewFromUtf8(isolate, escaped.c_str()));	
+
+	args.GetReturnValue().Set(obj);
 }
 
 void NodeExtractFromFile(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
 
-    v8::String::Utf8Value param(args[0]->ToString());
-	args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Hello World From Extract!"));
+    v8::String::Utf8Value param1(args[0]->ToString());
+    std::string filename(*param1);
+	
+    v8::String::Utf8Value param2(args[1]->ToString());
+	std::string schema(*param2);
+	
+    std::vector<std::vector<std::string>> table_vec;
+	// Extract
+	ExtractFromFile(filename, schema, &table_vec);
+
+    Local<Array> table = Array::New(isolate);
+    for (int i = 0; i < table_vec.size(); ++i) {
+        Local<Array> list = Array::New(isolate);
+        for (int j = 0; j < table_vec[i].size(); ++j)
+            list->Set(j, String::NewFromUtf8(isolate, table_vec[i][j].c_str()));    
+        table->Set(i, list);
+    }
+
+	args.GetReturnValue().Set(table);
 }
 
 void Init(Handle<Object> exports, Handle<Object> module) {
--- NodejsAddon/addon.h
+++ NodejsAddon/addon.h
@@ -1,3 +1,6 @@
 #include <string>
 #include <vector>
 
+void CandidateGenerate(const std::string& filename, std::vector<std::string>* vec, std::vector<std::string>* escaped);
+void SelectSchema(const std::string& filename, std::vector<std::string>& vec, std::string* schema, std::string* escaped);
+void ExtractFromFile(const std::string& filename, const std::string& schema, std::vector<std::vector<std::string>>* table);
